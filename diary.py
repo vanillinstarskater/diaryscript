ef5307db6b4338cc4f758d9e465ca2bc54430087
Vanillin Starskater <vanillinstarskater@gmail.com>
2025-09-19 05:36:02 -0700

    Initial (and probably only) commit

---
+++ diary.py
@@ -0,0 +1,11 @@
+import os
+import os.path
+from datetime import datetime
+
+current_file: str = f"/home/vanillin/.diary/{datetime.today().strftime("%Y-%m-%d")}.txt"
+if not os.path.exists(current_file):
+    with open("./header.txt", "r") as f:
+        header = f.read()
+    with open(current_file, "w") as f:
+        f.write(header)
+os.system(f"nvim {current_file}")
---
+++ readme.md
@@ -0,0 +1,1 @@
+A simple python script I use to keep my diary, because I don't need a "second brain" or anything like that, just a place to get my thoughts straight.
